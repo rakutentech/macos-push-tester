601d1e51b3846dc34c68e638f2c94181b16e0694
Alayli, Malik <ts-alayli.malik@rakuten.com>
2019-11-13 15:16:42 +0900

    APNS Identity Refactoring

--- APNS/APNS/APNSSecIdentityType.swift
+++ APNS/APNS/APNSSecIdentityType.swift
@@ -1,52 +1,54 @@
 import Foundation
 import Security
 
-// http://www.apple.com/certificateauthority/Apple_WWDR_CPS
-let APNSSecIdentityTypeDevelopmentCustomExtension = "1.2.840.113635.100.6.3.1"
-let APNSSecIdentityTypeProductionCustomExtension = "1.2.840.113635.100.6.3.2"
-let APNSSecIdentityTypeUniversalCustomExtension = "1.2.840.113635.100.6.3.6"
-
-enum APNSSecIdentityType: Int {
-    case invalid, development, production, universal
-}
-
-private func APNSecValuesForIndentity(_ identity: SecIdentity) -> [String: Any]? {
-    var certificate: SecCertificate?
+enum APNSSecIdentity {
+    // http://www.apple.com/certificateauthority/Apple_WWDR_CPS
+    static let developmentCustomExtension = "1.2.840.113635.100.6.3.1"
+    static let productionCustomExtension = "1.2.840.113635.100.6.3.2"
+    static let universalCustomExtension = "1.2.840.113635.100.6.3.6"
     
-    SecIdentityCopyCertificate(identity, &certificate)
-    
-    guard let cert = certificate else {
-        return [:]
+    enum APNSSecIdentityType: Int {
+        case invalid, development, production, universal
     }
     
-    let keys: NSArray = [
-        APNSSecIdentityTypeDevelopmentCustomExtension,
-        APNSSecIdentityTypeProductionCustomExtension,
-        APNSSecIdentityTypeUniversalCustomExtension,
-    ]
-    
-    let values: [String: Any]? = SecCertificateCopyValues(cert, keys, nil) as? [String: Any]
-    
-    certificate = nil
-    
-    return values
-}
-
-func APNSSecIdentityGetType(_ identity: SecIdentity) -> APNSSecIdentityType {
-    guard let values = APNSecValuesForIndentity(identity) else {
-        return .invalid
+    private static func values(for identity: SecIdentity) -> [String: Any]? {
+        var certificate: SecCertificate?
+        
+        SecIdentityCopyCertificate(identity, &certificate)
+        
+        guard let cert = certificate else {
+            return [:]
+        }
+        
+        let keys: NSArray = [
+            developmentCustomExtension,
+            productionCustomExtension,
+            universalCustomExtension,
+        ]
+        
+        let values: [String: Any]? = SecCertificateCopyValues(cert, keys, nil) as? [String: Any]
+        
+        certificate = nil
+        
+        return values
     }
     
-    if (values[APNSSecIdentityTypeDevelopmentCustomExtension] != nil) && (values[APNSSecIdentityTypeProductionCustomExtension] != nil) {
-        return .universal
+    static func type(for identity: SecIdentity) -> APNSSecIdentityType {
+        guard let values = values(for: identity) else {
+            return .invalid
+        }
         
-    } else if (values[APNSSecIdentityTypeDevelopmentCustomExtension]) != nil {
-        return .development
-        
-    } else if (values[APNSSecIdentityTypeProductionCustomExtension]) != nil {
-        return .production
-        
-    } else {
-        return .invalid
+        if (values[developmentCustomExtension] != nil) && (values[productionCustomExtension] != nil) {
+            return .universal
+            
+        } else if (values[developmentCustomExtension]) != nil {
+            return .development
+            
+        } else if (values[productionCustomExtension]) != nil {
+            return .production
+            
+        } else {
+            return .invalid
+        }
     }
 }
